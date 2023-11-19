d3254a03e8efdd01b4b07961116e272154804b62
Elba <elba.duishekeeva16@gmail.com>
2023-11-19 14:30:47 +0600

    card

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+const Content = () => {
+    return (
+        <div>
+            <h3>Always in style!</h3>
+            <p className={'overview'}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus
+                perspiciatis voluptatem?
+            </p>
+        </div>
+    )
+}
+
+export default Content;
